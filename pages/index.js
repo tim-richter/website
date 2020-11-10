3188fe069aa5aee6b41dadae7dc3d48d21b36438
Tim Richter <tim.richter@ganztags.net>
2020-11-10 21:16:56 +0100

    Init

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
+import React from "react";
+
+
+export default function Home() {
+  return <Title>My page</Title>
+}
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
-
-export default function Home() {
-  return <Title>My page</Title>
-}
