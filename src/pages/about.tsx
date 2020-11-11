9a62d4c62ca35a36026f0112ca68426f655c0626
Tim Richter <tim@timrichter.dev>
2020-11-11 11:26:35 +0100

    feat: add basic about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BaseLayout from '../layouts/BaseLayout';
+
+const About = () => {
+  return <BaseLayout>This is the layout page</BaseLayout>;
+};
+
+export default About;
