4e365f842e721e0c60b5c7f065b01ec194d07e83
Tim Richter <tim@timrichter.dev>
2020-11-11 14:03:44 +0100

    style: add global box-sizing and img styling

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,11 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-family: 'Lato', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
   }
 `;
 
