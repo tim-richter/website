c6c013610eb1024ae5511c0f6591b1d63c52b796
Tim Richter <tim@timrichter.dev>
2020-11-11 11:22:02 +0100

    feat: add custom Link component with styled component

---
+++ src/components/Link.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import React from 'react';
+import { darken } from 'polished';
+
+type Props = {
+  as?: any;
+  children: any;
+  className?: string;
+  href: string;
+};
+
+const StyledLink = ({ as, children, className, href }: Props) => (
+  <Link href={href} as={as} passHref>
+    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+    <a className={className}>{children}</a>
+  </Link>
+);
+
+export default styled(StyledLink)`
+  color: ${({ theme }) => theme.colors.primary};
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    color: ${({ theme }) => darken(0.3, theme.colors.primary)};
+  }
+
+  &:focus {
+    color: ${({ theme }) => darken(0.3, theme.colors.primary)};
+    outline: none;
+    border: 0;
+  }
+`;
