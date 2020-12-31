9bb4761d35454a0edde271102a2df75616c8511f
Tim Richter <tim@timrichter.dev>
2020-12-31 01:44:17 +0100

    feat: add ackee tracking script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import React from 'react';
 
-export default class MyDocument extends Document {
+interface Props {
+  isProduction: boolean;
+}
+
+export default class MyDocument extends Document<Props> {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -13,9 +17,12 @@ export default class MyDocument extends Document {
           enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
 
+      const isProduction = process.env.NODE_ENV === 'production';
+
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
+        isProduction,
         styles: (
           <>
             {initialProps.styles}
@@ -29,9 +36,20 @@ export default class MyDocument extends Document {
   }
 
   render() {
+    const { isProduction } = this.props;
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {isProduction && (
+            <script
+              async
+              src="https://ackee.timrichter.dev/tracker.js"
+              data-ackee-server="https://ackee.timrichter.dev"
+              data-ackee-domain-id="0f1a7f3e-4055-4caf-a374-952ca34a03c3"
+            />
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
