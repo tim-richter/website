cc3f5953553e6482620e745862b19011e8fa08dc
Tim Richter <tim@timrichter.dev>
2021-01-18 21:31:04 +0100

    #13 fix: render test failed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import * as nextRouter from 'next/router';
+import { RouterContext } from 'next/dist/next-server/lib/router-context';
 import StyledComponent from './styledComponent';
 import App from '../pages';
+
+nextRouter.useRouter = jest.fn();
+nextRouter.useRouter.mockImplementation(() => ({ route: '/' }));
 
 describe('App', () => {
   it('renders without crashing', () => {
     render(
-      <StyledComponent>
-        <App />
-      </StyledComponent>
+      <RouterContext.Provider value={nextRouter}>
+        <StyledComponent>
+          <App />
+        </StyledComponent>
+      </RouterContext.Provider>
     );
-    expect(screen.getByText('My page')).toBeInTheDocument();
+    expect(screen.getByText('About')).toBeInTheDocument();
   });
 });
