75060b6442a898ac938e4030f64cb074e2218529
Tim Richter <tim@timrichter.dev>
2021-01-20 18:12:36 +0100

    #20 feat: add cypress basic tests

---
+++ cypress/integration/home.ts
@@ -0,0 +1,19 @@
+Cypress.on('window:before:load', (win) => {
+  cy.spy(win.console, 'error');
+  cy.spy(win.console, 'warn');
+});
+
+describe('Homepage', () => {
+  it('should render', () => {
+    cy.visit('/');
+    cy.get('header').should('be.visible');
+  });
+
+  it('should have no console errors or warnings', () => {
+    cy.visit('/');
+    cy.window().then((win) => {
+      expect(win.console.error).to.have.callCount(0);
+      expect(win.console.warn).to.have.callCount(0);
+    });
+  });
+});
--- src/pages/about.test.tsx
+++ src/pages/about.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import StyledComponent from '../tests/styledComponent';
-import About from './About';
+import About from './about';
 
 describe('PAGE About', () => {
   it('should render without crashing', () => {
