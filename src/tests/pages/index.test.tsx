d17c3d5445d970722c46493a3d8b1c2eb3094946
Tim Richter <tim@timrichter.dev>
2021-02-10 12:26:20 +0100

    fix: add page tests to seperate folder

--- src/tests/pages/index.test.tsx
+++ src/tests/pages/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import StyledComponent from '../../tests/styledComponent';
-import Blog from '.';
+import StyledComponent from '../styledComponent';
+import Blog from '../../pages/blog';
 
 describe('PAGE blog/', () => {
   it('should render without crashing', () => {
--- src/tests/pages/legal-notice.test.tsx
+++ src/tests/pages/legal-notice.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import StyledComponent from '../tests/styledComponent';
-import LegalNotice from './legal-notice';
+import StyledComponent from '../styledComponent';
+import LegalNotice from '../../pages/legal-notice';
 
 describe('PAGE Legal Notice', () => {
   it('should render without crashing', () => {
